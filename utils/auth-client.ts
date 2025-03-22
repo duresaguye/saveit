83f9ff3152007af8f20e08a6f750c1fa68948d0d
duresaguye <dureguye2@gmail.com>
2025-03-22 21:58:44 +0300

    feat: github autha and google auth added

--- utils/auth-client.ts
+++ utils/auth-client.ts
@@ -1,5 +1,15 @@
 import { createAuthClient } from "better-auth/react" 
  
-export const authClient =  createAuthClient({
-    //you can pass client configuration here
-})
+export const authClient = createAuthClient({});
+
+const signIn = async () => {
+    const githubData = await authClient.signIn.social({
+        provider: "github",
+    });
+
+    const googleData = await authClient.signIn.social({
+        provider: "google",
+    });
+
+    const data = { github: githubData, google: googleData };
+};
--- utils/auth.ts
+++ utils/auth.ts
@@ -5,9 +5,19 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
+        provider: "postgresql", 
     }),
     emailAndPassword: {    
         enabled: true
-    } 
+    },
+    socialProviders: {
+        github: { 
+            clientId: process.env.GITHUB_CLIENT_ID as string, 
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+        }, 
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
 });
